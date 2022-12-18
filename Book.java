d65f8b0c79f6dfa60dadf640a53e5f972104407a
jamcuw <112746094+jamcuw@users.noreply.github.com>
2022-12-18 23:00:43 +0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,18 @@
+package BBbook;
+
+public class Book {
+	
+	String nname;
+	int numchapters;
+	String desc;
+	
+	Book(String nname, int numchapters, String desc){
+		
+		this.nname = nname;
+		this.numchapters = numchapters;
+		this.desc = desc;
+		
+		
+		
+	}
+}
