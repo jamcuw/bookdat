c81351b36d42094edfa41ce303ea684192471a9b
jamcuw <112746094+jamcuw@users.noreply.github.com>
2022-12-18 23:00:14 +0800

    Add files via upload

---
+++ bookproj.java
@@ -0,0 +1,42 @@
+package BBbook;
+
+public class bookproj {
+	public static void main(String[] args) {
+
+		Book bk = new Book("Matthew",28,"The Gospel according to Matthew");
+		Book bk2 = new Book("Mark", 16, "The Gospel according to Mark.");
+		Book bk3 = new Book("Luke", 24, "The Gospel according to Luke.");
+		Book bk4 = new Book("John" ,21, "The Gospel according to John.");
+		Book bk5 = new Book("Acts" , 28 , "Acts of the Apostles.");
+		Book bk6 = new Book("Romans", 16, "Letter of Paul to the Romans");
+		Book bk7 = new Book("Corinthians I", 16 , "First Letter of Paul to the Corinthians.");
+		Book bk8 = new Book("Corinthians II", 13 , "Second Letter of Paul to the Corinthians.");
+		Book bk9 = new Book("Galatians", 6 , "Letter of Paul to the Galatians.");
+		Book bk10 = new Book("Ephesians", 6 , "Letter of Paul to the Ephesians.");
+		Book bk11 = new Book("Philippians", 4 , "Letter of Paul to the Philippians.");
+		Book bk12 = new Book("Colossians", 4 , "Letter of Paul to the Colossians.");
+		Book bk13 = new Book("Thessalonians I", 5 , "First Letter of Paul to the Thessalonians.");
+		Book bk14 = new Book("Thessalonians II", 4 , "Second Letter of Paul to the Thessalonians.");
+		Book bk15 = new Book("Timothy I", 6 , "First Letter of Paul to Timothy.");
+		Book bk16 = new Book("Timothy II", 4 , "Second Letter of Paul to TImothy.");
+		Book bk17 = new Book("Titus", 3 , "Letter of Paul to Titus.");
+		Book bk18 = new Book("Philemon", 1 , "Letter of Paul to Philemon.");
+		Book bk19 = new Book ("Hebrews", 13, "Letter to the Hebrews.");
+		Book bk20 = new Book ("James", 5 , "Letter of James.");
+		Book bk21 = new Book ("Peter I", 5 , "First Letter of Peter.");
+		Book bk22 = new Book ("Peter II", 3 , "Second Letter of Peter.");
+		Book bk23 = new Book ("John I", 5 , "First Letter of John.");
+		Book bk24 = new Book ("John II", 1 , "Second Letter of John.");
+		Book bk25 = new Book ("John III", 1 , "Third Letter of John.");
+		Book bk26 = new Book ("Jude", 1 , "Letter of Jude.");
+		Book bk27 = new Book ("Revelation", 22 , "The Book of Revelation.");
+		
+		
+		
+		
+		System.out.println(bk22.nname);
+		System.out.println(bk22.numchapters);
+		System.out.println(bk22.desc);
+	
+	}
+}
